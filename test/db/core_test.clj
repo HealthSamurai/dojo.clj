7b81921c90130ff8148d3fc0c9a9a3c3a90c4ec5
niquola <niquola@gmail.com>
2019-09-21 12:28:49 +0300

    add db code

---
+++ test/db/core_test.clj
@@ -0,0 +1,44 @@
+(ns db.core-test
+  (:require [db.core :as sut]
+            [matcho.core :as matcho]
+            [clojure.test :refer :all]))
+
+(deftest db.core-test
+  (def db-spec (sut/db-spec-from-env))
+  (defonce db (sut/connection db-spec))
+
+  (matcho/match
+   (sut/query db "select 1 a")
+   [{:a 1}])
+
+  (matcho/match
+   (sut/query-first db "select 1 a")
+   {:a 1})
+
+  (matcho/match
+   (sut/query-value db "select 1 a")
+   1)
+
+  (sut/table-exists? db "users")
+  (sut/table-exists? db "informational_schema.tables")
+
+  (sut/exec! db
+             "
+drop table if exists tests;
+create table tests (
+ id serial primary key,
+ resource jsonb
+);
+
+")
+
+  (sut/insert db {:table :tests} {:id 1 :resource {:name "John"}})
+
+  (sut/update db {:table :tests} {:id 1 :resource {:name "Ivan"}})
+
+  (matcho/match
+   (sut/query db {:select [:*] :from [:tests]})
+   [{:id 1 :resource {:name "Ivan"}}])
+
+  )
+
