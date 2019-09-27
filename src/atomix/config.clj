9f6d9bed4f0777571c98f107a242ae51f71b3722
Mike Ananev <mike1452@yandex.ru>
2019-09-27 20:44:19 +0300

    added atomix storage

--- src/atomix/config.clj
+++ src/atomix/config.clj
@@ -1,11 +1,33 @@
-(ns atomix.config)
+(ns atomix.config
+  (:import [io.atomix.protocols.raft.storage RaftStorage]
+           (io.atomix.storage StorageLevel)))
 
-(def *state (atom nil))
+(def *node-conf1 (atom nil))
+
+(def *node-conf2 (atom nil))
+
+(def *node-conf3 (atom nil))
 
 (defn read-config
   "return edn config loaded from file"
   [conf-name]
   (-> conf-name slurp clojure.edn/read-string))
+
+(defn storage
+  "configure storage for atomix"
+  [{:keys [:storage/type :storage/folder]}]
+  (case type
+    :disk (-> (RaftStorage/builder)
+              (.withDirectory folder)
+              (.withStorageLevel (StorageLevel/DISK))
+              (.build))
+    :mmap-file (-> (RaftStorage/builder)
+                   (.withDirectory folder)
+                   (.withStorageLevel (StorageLevel/MAPPED))
+                   (.build))
+    :memory (-> (RaftStorage/builder)
+                (.withStorageLevel (StorageLevel/MEMORY))
+                (.build))))
 
 (comment
   )
